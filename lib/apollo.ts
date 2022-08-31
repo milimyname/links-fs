f3f7107d8923065a8e99965156171f23c0f6adf7
mili <kamil.maksudov2019@gmail.com>
2022-08-31 02:34:19 +0200

    Uri Apollo Bug Fixed

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -3,7 +3,7 @@ import { relayStylePagination } from '@apollo/client/utilities';
 
 const apolloClient = new ApolloClient({
     // It'll be replaced with the real endpoint when the app is deployed.
-    uri: 'https://links-fs.vercel.app/api/graphql',
+    uri: 'api/graphql',
     cache: new InMemoryCache({
         typePolicies:{
             Query:{
